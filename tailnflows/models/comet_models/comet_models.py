9e6929d8a92b9dfbcbf4df5932b0137181e7d3ae
tennessee_wallaceh <tennessee.wallaceh@gmail.com>
2024-02-07 10:54:54 +0000

    Tidy

--- tailnflows/models/comet_models/comet_models.py
+++ tailnflows/models/comet_models/comet_models.py
@@ -1,10 +1,10 @@
 """
 Replicated from 
 """
+
 import numpy as np
 import torch
 import torch.nn as nn
-import pytorch_lightning as pl
 import scipy.stats
 import scipy.optimize
 import statsmodels.api as sm
@@ -521,255 +521,99 @@ class CouplingLayer(nn.Module):
             return y, logpx + delta_logp
 
 
-class BaseFlow(pl.LightningModule):
-    """
-    Base normalizing flow model.
-    Inspired by ffjord.lib.layers.container at https://github.com/rtqichen/ffjord.
-    """
+# class VanillaFlow(BaseFlow):
+#     """
+#     Vanilla coupling layer-based normalizing flow model.
+#     Inspired by Dinh et al. RealNVP.
+#     """
 
-    def __init__(self, d, hidden_ds, lr):
-        super().__init__()
-        self.save_hyperparameters()
-        self.d = d
-        self.hidden_ds = hidden_ds
-        self.lr = lr
-        self.conditional_noise = False
-        self.layers = []
-
-    def forward(self, x, noise_level=None, logpx=None, reverse=False):
-        if logpx is None:
-            logpx = torch.zeros(x.shape[0], 1, device=self.device)
-
-        if reverse:
-            layers = reversed(self.layers)
-        else:
-            layers = self.layers
-
-        for layer in layers:
-            x, logpx = layer(x, noise_level, logpx, reverse)
-        return x, logpx
-
-    def training_step(self, batch, batch_idx):
-        self.train()
-        x = batch["x"].type(torch.FloatTensor).to(self.device)
-        criterion = self.get_criterion()
-
-        # perform forward pass
-        if self.conditional_noise:
-            std_min, std_max = 1e-3, 1e-1
-            noise = (std_max - std_min) * torch.rand_like(x[:, 0]).view(-1, 1) + std_min
-            noisy_x = x + torch.randn_like(x) * noise
-            z, delta_logp = self.forward(noisy_x, noise_level=noise)
-        else:
-            z, delta_logp = self.forward(x)
-
-        # check nans and infs
-        nan_idx = torch.all(torch.logical_or(z != z, delta_logp != delta_logp), dim=1)
-        inf_idx = torch.all(
-            torch.logical_not(
-                torch.logical_and(torch.isfinite(z), torch.isfinite(delta_logp))
-            ),
-            dim=1,
-        )
-        keep_idx = torch.logical_not(torch.logical_or(nan_idx, inf_idx))
-        z, delta_logp = z[keep_idx], delta_logp[keep_idx]
-
-        # compute and log mean nll
-        nll = criterion(z, delta_logp) / z.shape[0]
-        self.log("t_loss", nll, prog_bar=True)
-        self.log("t_nan", torch.sum(nan_idx), prog_bar=True)
-        self.log("t_inf", torch.sum(inf_idx), prog_bar=True)
-        return nll
-
-    def log_prob(self, x):
-        criterion = self.get_criterion()
-
-        # perform forward pass
-        if self.conditional_noise:
-            std_min, std_max = 1e-3, 1e-1
-            noise = (std_max - std_min) * torch.rand_like(x[:, 0]).view(-1, 1) + std_min
-            noisy_x = x + torch.randn_like(x) * noise
-            z, delta_logp = self.forward(noisy_x, noise_level=noise)
-        else:
-            z, delta_logp = self.forward(x)
-
-        # check nans and infs
-        nan_idx = torch.all(torch.logical_or(z != z, delta_logp != delta_logp), dim=1)
-        inf_idx = torch.all(
-            torch.logical_not(
-                torch.logical_and(torch.isfinite(z), torch.isfinite(delta_logp))
-            ),
-            dim=1,
-        )
-        keep_idx = torch.logical_not(torch.logical_or(nan_idx, inf_idx))
-        z, delta_logp = z[keep_idx], delta_logp[keep_idx]
-
-        # compute and log mean nll
-        nll = criterion(z, delta_logp) / z.shape[0]
-        return -nll
-
-    def validation_step(self, batch, batch_idx):
-        self.eval()
-        x = batch["x"].type(torch.FloatTensor).to(self.device)
-        criterion = self.get_criterion()
-
-        # perform forward pass
-        if self.conditional_noise:
-            std_min, std_max = 1e-3, 5e-3  # set to near-zero noise for validation
-            noise = (std_max - std_min) * torch.rand_like(x[:, 0]).view(-1, 1) + std_min
-            noisy_x = x + torch.randn_like(x) * noise
-            z, delta_logp = self.forward(noisy_x, noise_level=noise)
-        else:
-            z, delta_logp = self.forward(x)
-
-        # check nans and infs
-        nan_idx = torch.all(torch.logical_or(z != z, delta_logp != delta_logp), dim=1)
-        inf_idx = torch.all(
-            torch.logical_not(
-                torch.logical_and(torch.isfinite(z), torch.isfinite(delta_logp))
-            ),
-            dim=1,
-        )
-        keep_idx = torch.logical_not(torch.logical_or(nan_idx, inf_idx))
-        z, delta_logp = z[keep_idx], delta_logp[keep_idx]
-
-        # compute and log mean nll
-        nll = criterion(z, delta_logp) / z.shape[0]
-        return {
-            "loss": nll,
-            "n_nan": torch.sum(nan_idx).type(torch.FloatTensor),
-            "n_inf": torch.sum(inf_idx).type(torch.FloatTensor),
-        }
-
-    def validation_epoch_end(self, outputs):
-        nll = torch.stack([o["loss"] for o in outputs]).mean()
-        self.log("v_loss", nll, prog_bar=True)
-        n_nan = torch.stack([o["n_nan"] for o in outputs]).mean()
-        self.log("v_nan", n_nan, prog_bar=True)
-        n_inf = torch.stack([o["n_inf"] for o in outputs]).mean()
-        self.log("v_inf", n_inf, prog_bar=True)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.lr)
-
-    def get_criterion(self):
-        return GaussianNLL(self.device)
-
-    def get_z_samples(self, n_samples):
-        return torch.randn((n_samples, self.d), device=self.device)
-
-    def n_parameters(self):
-        return sum(p.numel() for p in self.parameters() if p.requires_grad)
-
-    def sample(self, n_samples):
-        self.eval()
-        z = self.get_z_samples(n_samples)
-
-        # perform reverse pass
-        if self.conditional_noise:
-            std_min, std_max = 1e-3, 5e-3  # set to near-zero noise for validation
-            noise = (std_max - std_min) * torch.rand_like(z[:, 0]).view(-1, 1) + std_min
-            noisy_z = z + torch.randn_like(z) * noise
-            x, _ = self.forward(noisy_z, noise_level=noise, reverse=True)
-        else:
-            x, _ = self.forward(z, reverse=True)
-
-        return x
+#     def __init__(self, d, hidden_ds, lr):
+#         super().__init__(d, hidden_ds, lr)
+#         for hidden_d in hidden_ds:
+#             self.layers.append(CouplingLayer(self.d, hidden_d, swap=False))
+#             self.layers.append(CouplingLayer(self.d, hidden_d, swap=True))
+#         self.layers = nn.ModuleList(self.layers)
 
 
-class VanillaFlow(BaseFlow):
-    """
-    Vanilla coupling layer-based normalizing flow model.
-    Inspired by Dinh et al. RealNVP.
-    """
+# class TAFlow(BaseFlow):
+#     """
+#     Coupling layer-based normalizing flow model with Student's T latent space for heavy tailed modeling.
+#     Inspired by Jaini et al. Tail Adaptive Flow.
+#     """
 
-    def __init__(self, d, hidden_ds, lr):
-        super().__init__(d, hidden_ds, lr)
-        for hidden_d in hidden_ds:
-            self.layers.append(CouplingLayer(self.d, hidden_d, swap=False))
-            self.layers.append(CouplingLayer(self.d, hidden_d, swap=True))
-        self.layers = nn.ModuleList(self.layers)
+#     def __init__(self, d, hidden_ds, lr, dof):
+#         super().__init__(d, hidden_ds, lr)
+#         for hidden_d in hidden_ds:
+#             self.layers.append(CouplingLayer(self.d, hidden_d, swap=False))
+#             self.layers.append(CouplingLayer(self.d, hidden_d, swap=True))
+#         self.layers = nn.ModuleList(self.layers)
+#         self.dof = dof
 
+#     def get_criterion(self):
+#         return StudentNLL(self.device, self.dof)
 
-class TAFlow(BaseFlow):
-    """
-    Coupling layer-based normalizing flow model with Student's T latent space for heavy tailed modeling.
-    Inspired by Jaini et al. Tail Adaptive Flow.
-    """
-
-    def __init__(self, d, hidden_ds, lr, dof):
-        super().__init__(d, hidden_ds, lr)
-        for hidden_d in hidden_ds:
-            self.layers.append(CouplingLayer(self.d, hidden_d, swap=False))
-            self.layers.append(CouplingLayer(self.d, hidden_d, swap=True))
-        self.layers = nn.ModuleList(self.layers)
-        self.dof = dof
-
-    def get_criterion(self):
-        return StudentNLL(self.device, self.dof)
-
-    def get_z_samples(self, n_samples):
-        samples = scipy.stats.multivariate_t(
-            loc=np.zeros(
-                self.d,
-            ),
-            shape=np.eye(self.d),
-            df=self.dof,
-        ).rvs(size=n_samples)
-        return torch.from_numpy(samples).type(torch.FloatTensor).to(self.device)
+#     def get_z_samples(self, n_samples):
+#         samples = scipy.stats.multivariate_t(
+#             loc=np.zeros(
+#                 self.d,
+#             ),
+#             shape=np.eye(self.d),
+#             df=self.dof,
+#         ).rvs(size=n_samples)
+#         return torch.from_numpy(samples).type(torch.FloatTensor).to(self.device)
 
 
-class CMFlow(BaseFlow):
-    """
-    Coupling layer-based normalizing flow model with copula transform for heavy tail modeling.
-    Inspired by Wiese et al. Copula & Marginal Flows.
-    """
+# class CMFlow(BaseFlow):
+#     """
+#     Coupling layer-based normalizing flow model with copula transform for heavy tail modeling.
+#     Inspired by Wiese et al. Copula & Marginal Flows.
+#     """
 
-    def __init__(self, d, hidden_ds, lr, data, a, b):
-        super().__init__(d, hidden_ds, lr)
-        self.layers.append(MarginalLayer(data, a=a, b=b))  # map to unit hypercube
-        self.layers.append(LogitLayer())  # map to Rn before coupling layers
-        for hidden_d in hidden_ds:
-            self.layers.append(CouplingLayer(self.d, hidden_d, swap=False))
-            self.layers.append(CouplingLayer(self.d, hidden_d, swap=True))
-        self.layers = nn.ModuleList(self.layers)
-
-
-class SoftFlow(BaseFlow):
-    """
-    Coupling layer-based normalizing flow model with conditional noise auxiliary variable for manifold modeling.
-    Inspired by Kim et al. SoftFlow.
-    """
-
-    def __init__(self, d, hidden_ds, lr):
-        super().__init__(d, hidden_ds, lr)
-        self.conditional_noise = True
-        for hidden_d in hidden_ds:
-            self.layers.append(
-                CouplingLayer(self.d, hidden_d, swap=False, conditional_noise=True)
-            )
-            self.layers.append(
-                CouplingLayer(self.d, hidden_d, swap=True, conditional_noise=True)
-            )
-        self.layers = nn.ModuleList(self.layers)
+#     def __init__(self, d, hidden_ds, lr, data, a, b):
+#         super().__init__(d, hidden_ds, lr)
+#         self.layers.append(MarginalLayer(data, a=a, b=b))  # map to unit hypercube
+#         self.layers.append(LogitLayer())  # map to Rn before coupling layers
+#         for hidden_d in hidden_ds:
+#             self.layers.append(CouplingLayer(self.d, hidden_d, swap=False))
+#             self.layers.append(CouplingLayer(self.d, hidden_d, swap=True))
+#         self.layers = nn.ModuleList(self.layers)
 
 
-class COMETFlow(BaseFlow):
-    """
-    Original coupling layer-based normalizing flow model with copula transform for heavy tail modeling and
-    with conditional noise auxiliary variable for manifold modeling.
-    """
+# class SoftFlow(BaseFlow):
+#     """
+#     Coupling layer-based normalizing flow model with conditional noise auxiliary variable for manifold modeling.
+#     Inspired by Kim et al. SoftFlow.
+#     """
 
-    def __init__(self, d, hidden_ds, lr, data, a, b):
-        super().__init__(d, hidden_ds, lr)
-        self.conditional_noise = True
-        self.layers.append(MarginalLayer(data, a=a, b=b))  # map to unit hypercube
-        self.layers.append(LogitLayer())  # map to Rn before coupling layers
-        for hidden_d in hidden_ds:
-            self.layers.append(
-                CouplingLayer(self.d, hidden_d, swap=False, conditional_noise=True)
-            )
-            self.layers.append(
-                CouplingLayer(self.d, hidden_d, swap=True, conditional_noise=True)
-            )
-        self.layers = nn.ModuleList(self.layers)
+#     def __init__(self, d, hidden_ds, lr):
+#         super().__init__(d, hidden_ds, lr)
+#         self.conditional_noise = True
+#         for hidden_d in hidden_ds:
+#             self.layers.append(
+#                 CouplingLayer(self.d, hidden_d, swap=False, conditional_noise=True)
+#             )
+#             self.layers.append(
+#                 CouplingLayer(self.d, hidden_d, swap=True, conditional_noise=True)
+#             )
+#         self.layers = nn.ModuleList(self.layers)
+
+
+# class COMETFlow(BaseFlow):
+#     """
+#     Original coupling layer-based normalizing flow model with copula transform for heavy tail modeling and
+#     with conditional noise auxiliary variable for manifold modeling.
+#     """
+
+#     def __init__(self, d, hidden_ds, lr, data, a, b):
+#         super().__init__(d, hidden_ds, lr)
+#         self.conditional_noise = True
+#         self.layers.append(MarginalLayer(data, a=a, b=b))  # map to unit hypercube
+#         self.layers.append(LogitLayer())  # map to Rn before coupling layers
+#         for hidden_d in hidden_ds:
+#             self.layers.append(
+#                 CouplingLayer(self.d, hidden_d, swap=False, conditional_noise=True)
+#             )
+#             self.layers.append(
+#                 CouplingLayer(self.d, hidden_d, swap=True, conditional_noise=True)
+#             )
+#         self.layers = nn.ModuleList(self.layers)
