ec0b52e334913ef5e6666e517d2fbaccbb23d2b4
tennessee_wallaceh <tennessee.wallaceh@gmail.com>
2024-04-09 14:21:11 +0100

    Update colab reqs to a py file

---
+++ tailnflows/colab_requirements.py
@@ -0,0 +1,12 @@
+pip_dependencies = [
+    "jupyter",
+    "matplotlib",
+    "numpy",
+    "scipy",
+    "tqdm",
+    "pandas",
+    "seaborn",
+    "posteriordb",
+    "git+https://github.com/Tennessee-Wallaceh/nflows",
+    "git+https://github.com/Tennessee-Wallaceh/marginalTailAdaptiveFlow.git",
+]
